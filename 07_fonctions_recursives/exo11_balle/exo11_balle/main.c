3d2b0ebc277ff8dc761730cf365906aeb8fe774b
flabastie <flabastie@hotmail.com>
2020-05-05 18:37:51 +0200

    exo 12 palindrome

--- 07_fonctions_recursives/exo11_balle/exo11_balle/main.c
+++ 07_fonctions_recursives/exo11_balle/exo11_balle/main.c
@@ -7,9 +7,26 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+float HauteurFinale(float, int);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    float h;
+    int nb;
+    printf("Saisir la hauteur: ");
+    scanf("%f",&h);
+    printf("Saisir le nombre de rebonds: ");
+    scanf("%d",&nb);
+    printf("Hauteur finale = %.2f m \n", HauteurFinale(h, nb));
     return 0;
+}
+
+float HauteurFinale(float h, int nb)
+{
+    if(nb==0)
+        return h;
+    else
+        return HauteurFinale(h/2.0, nb-1);
 }
